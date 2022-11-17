32b014e9ad8f9f0e91b119794fe96146adb69567
eduard <you@example.com>
2022-11-17 22:46:09 +0300

    Решена первая задача итогового проекта

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+void PrintNumbersDesc(int number)
+{
+    if (number == 1)
+    {
+        Console.Write(1);
+        return;
+    }
+    else
+    {
+        Console.Write(number + " ");
+        PrintNumbersDesc(number - 1);
+    }
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+PrintNumbersDesc(number);

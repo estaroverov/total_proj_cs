d078b566ec9757871bc03002032f950d42636e75
eduard <you@example.com>
2022-11-17 23:11:28 +0300

    Решена вторая задача

--- Program.cs
+++ Program.cs
@@ -14,6 +14,21 @@ void PrintNumbersDesc(int number)
         PrintNumbersDesc(number - 1);
     }
 }
+
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 PrintNumbersDesc(number);
+
+// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int CalcSumNumbers(int from, int end)
+{
+    if (from == end)
+        return from;
+    else
+        return CalcSumNumbers(from + 1, end) + from;
+}
+Console.WriteLine();
+Console.WriteLine("sum = " + CalcSumNumbers(1, 15));
